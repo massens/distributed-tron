7d49b1bb9fc93ba3d26384fe287eff262f290e29
Marc Assens <marc.a.r95@gmail.com>
2015-12-16 18:42:52 +0100

    Finestra 700x700

--- src/Client/Vista_Client.java
+++ src/Client/Vista_Client.java
@@ -1,6 +1,6 @@
 package Client;
 
-import Utils.ScreenManager;
+import Utils.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,63 +8,59 @@ import java.util.*;
 
 public class Vista_Client implements Observer{
 	Model_Client model;
-
-	private static final DisplayMode modes[] = 
-		{
-		//new DisplayMode(1920,1080,32,0),
-		new DisplayMode(1680,1050,32,0),
-		//new DisplayMode(1280,1024,32,0),
-		new DisplayMode(800,600,32,0),
-		new DisplayMode(800,600,24,0),
-		new DisplayMode(800,600,16,0),
-		new DisplayMode(640,480,32,0),
-		new DisplayMode(640,480,24,0),
-		new DisplayMode(640,480,16,0),
-		};
-
-	public ScreenManager sm;
-	protected Window w;
+	JFrame f;
+        PanellPintar panelDibuix;
 
 
 	public Vista_Client(Model_Client model){
 		this.model = model;
 		model.afegirVista(this);
 
-		sm = new ScreenManager();
-		DisplayMode dm = sm.findFirstCompatibaleMode(modes);
-		sm.setFullScreen(dm);
-		w = sm.getFullScreenWindow();
-		w.setFont(new Font("Arial",Font.PLAIN,20));
-		w.setBackground(Color.WHITE);
-		w.setForeground(Color.RED);
+		f = new JFrame("Tron");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(Const.SCREENX, Const.SCREENY);
+		panelDibuix = new PanellPintar();
+		f.add(panelDibuix);
+		//f.pack();
+		f.setVisible(true);
 
-		model.setWindowSize(sm.getWidth(), sm.getHeight());
+
+		model.setWindowSize(Const.SCREENX, Const.SCREENY);
 
 	}
 
 	public void update(Observable ignorarObs, Object ignorarObj){ //Pull update
 		
-        System.out.println("Vista Update!");
-		Graphics2D g = sm.getGraphics();
-
-
-		//Pintar
-		g.setColor(Color.BLACK); //Fica tot el fons a negre
-		g.fillRect(0, 0, sm.getWidth(), sm.getHeight());
-		for (int x = 0;x<model.getPathX1().size();x++){
-			g.setColor(Color.green);
-			g.fillRect(model.getPathX1().get(x), model.getPathY1().get(x), 10, 10);
-			g.setColor(Color.red);
-			g.fillRect(model.getPathX2().get(x), model.getPathY2().get(x), 10, 10);
-		}
-
-		sm.update();
+		f.repaint();
 
 	}
 
 
 	public void afegirControlador(KeyListener listener) { //Afegeix controladors
-		w.addKeyListener(listener);
+		f.addKeyListener(listener);
+	}
+
+
+	//--------------------------------------------
+	//				Panell Pintar
+	//--------------------------------------------
+
+
+	class PanellPintar extends JPanel{
+
+		@Override
+		public void paintComponent(Graphics g){
+			//Pintar
+			g.setColor(Color.BLACK); //Fica tot el fons a negre
+			g.fillRect(0, 0, Const.SCREENX, Const.SCREENY);
+			for (int x = 0;x<model.getPathX1().size();x++){
+				g.setColor(Color.green);
+				g.fillRect(model.getPathX1().get(x), model.getPathY1().get(x), Const.PATHSIZE, Const.PATHSIZE);
+				g.setColor(Color.red);
+				g.fillRect(model.getPathX2().get(x), model.getPathY2().get(x), Const.PATHSIZE, Const.PATHSIZE);
+			}
+		}
+
 	}
 	
 }
--- src/Utils/Const.java
+++ src/Utils/Const.java
@@ -6,5 +6,13 @@ public interface Const {
     public static final int DOWN = 2;
     public static final int LEFT = 3;
     public static final int NOACTION = 5;
+    
+    public static final int SCREENX = 700;
+    public static final int SCREENY = 700;
+    public static final int PATHSIZE = 10;
+
+
+    public static final int STEP = 5;
+
   
 }
